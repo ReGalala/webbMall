340f3c07dcef3930c13ddc930072316612239fec
rebake <rebake@net.chalmers.se>
2026-02-15 21:08:11 +0100

    Initial commit: Webmall

---
+++ client/src/views/Store.vue
@@ -0,0 +1,56 @@
+<!--<template>-->
+<!--  <link rel="stylesheet"-->
+<!--        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">-->
+
+<!--  <div>-->
+<!--    <b-container fluid>-->
+<!--      <div class="singleStore-container">-->
+<!--        <div id="singleStore-card">-->
+<!--          <img id="store-image" :src="`../public/store_images/${store.logo_img}`" alt="Store logo_img"/>-->
+<!--          <h3 id="store-name">{{ store.name }}</h3>-->
+<!--          <p id="store-location"><strong>Store Location: </strong><s> {{ store.location }}</s></p>-->
+<!--          <p id="store-org-number"><strong>Store Organisation Number:</strong> {{ store.org_number }}</p>-->
+<!--          <p id="store-description"><strong>Store Description:</strong> {{ store.description }}</p>-->
+<!--          <p id="store-products"><strong>Products:</strong> {{ store.products }}</p>-->
+<!--          <p id="store-sellers"><strong>Sellers:</strong> {{ store.sellers }}</p>-->
+<!--          <div id="error_message" class="error_message">{{ error_message }}</div>-->
+<!--        </div>-->
+<!--      </div>-->
+
+<!--    </b-container>-->
+
+
+<!--  </div>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--// @ is an alias to /src-->
+<!--import { Api } from '@/Api'-->
+
+<!--export default {-->
+<!--  name: 'singlestore',-->
+<!--  data() {-->
+<!--    return {-->
+<!--      message: 'none',-->
+<!--      store: {-->
+<!--        store_name: "",-->
+<!--        location: "",-->
+<!--        org_number: 0,-->
+<!--        description: "",-->
+<!--        sellers: "",-->
+<!--        logo_img: ""-->
+<!--      } ,-->
+<!--      confirmation_message: '',-->
+<!--      error_message: '',-->
+<!--    }-->
+<!--  },-->
+<!--  mounted() {-->
+
+<!--  },-->
+<!--  methods: {-->
+
+<!--  }-->
+<!--}-->
+<!--</script>-->
+
+<!--<style src="../css/style.css"></style>-->
